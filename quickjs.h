11147411027f81aad7e6bbb7e8c58f865e4cd674
Hayden Allen <hsa190003@utdallas.edu>
2023-12-10 22:49:56 -0600

    works on windows

--- quickjs.h
+++ quickjs.h
@@ -337,8 +337,13 @@ typedef struct JSValue {
 #define JS_VALUE_GET_FLOAT64(v) ((v).u.float64)
 #define JS_VALUE_GET_PTR(v) ((v).u.ptr)
 
+#ifdef QJS_USE_CPP
+#define JS_MKVAL(tag, val) JSValue { JSValueUnion { .int32 = val }, tag }
+#define JS_MKPTR(tag, p) JSValue { JSValueUnion { .ptr = p }, tag }
+#else
 #define JS_MKVAL(tag, val) (JSValue){ (JSValueUnion){ .int32 = val }, tag }
 #define JS_MKPTR(tag, p) (JSValue){ (JSValueUnion){ .ptr = p }, tag }
+#endif
 
 #define JS_TAG_IS_FLOAT64(tag) ((unsigned)(tag) == JS_TAG_FLOAT64)
 
